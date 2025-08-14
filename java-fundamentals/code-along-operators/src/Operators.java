19abc8a8daac78b767c9cfcb379ff149fa0bc536
Jaha Knight <jaha.knight@mac.mynetworksettings.com>
2025-08-14 12:20:05 -0400

    pushing homework

---
+++ java-fundamentals/code-along-operators/src/Operators.java
@@ -0,0 +1,42 @@
+public class Operators {
+
+    public static void main(String[] args) {
+        System.out.println("*******************Code Along Operators*******************");
+        System.out.println("*******************Assignment Operator*******************");
+        int x = 150;
+        System.out.println("The Value of x is" + x);
+        String name = "Jaha";
+        System.out.println("My name is " + name);
+
+        System.out.println("*******************Assignment Operator*******************");
+//      PEDMAS
+//      Please  - Parenthesis First (...)
+//      Excuse  - Exponents x^2
+//      My      - Multiplication
+//      Dear    - Division
+//      Aunt    - Addition
+//      Sally   - Subtraction
+        System.out.println("Arithmetic");
+          x = x+2;
+          System.out.println("x + 2 = " + x);
+          x+=2;
+          System.out.println("x+=2 = " + x);
+          x++;
+          System.out.println("x++ = " + x);
+
+          System.out.println("Subtraction");
+          x = x -2;
+          System.out.println("x -2 = " + x);
+          x--;
+          System.out.println("x-- =" + x);
+
+
+
+
+
+
+
+
+
+    }
+}

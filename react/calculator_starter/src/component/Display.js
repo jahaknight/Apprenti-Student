6a996aa4f958ed272e2ad214c7c85730d2448d42
jahaknight <jahaknight1@gmail.com>
2025-10-29 15:34:01 -0400

    Calculator Exercise

--- react/calculator_starter/src/component/Display.js
+++ react/calculator_starter/src/component/Display.js
@@ -4,5 +4,20 @@ import PropTypes from "prop-types";
 import "./Display.css";
 
 export default class Display extends React.Component {
-  
+    static propTypes = {
+        value: PropTypes.string,
+    };
+
+    static defaultProps = {
+        value: "0",
+    };
+
+    render() {
+        const { value } = this.props;
+        return (
+          <div className="component-display">
+            <div>{value}</div>
+          </div>
+        );
+      }    
 }

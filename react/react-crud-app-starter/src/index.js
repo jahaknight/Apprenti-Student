156bc5532ffb73e4351b645673e5162b8f9dcd81
jahaknight <jahaknight1@gmail.com>
2025-10-29 14:04:28 -0400

    Updated

--- react/react-crud-app-starter/src/index.js
+++ react/react-crud-app-starter/src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import './index.css';
+import APP from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
+);
 
